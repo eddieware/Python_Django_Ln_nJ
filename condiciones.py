e11cc671d6d3e73c9b4ce8483d73e96c41cf0072
Eduardo <ing.eduardo.mejia.bautista@gmail.com>
2019-09-07 21:23:32 -0500

    carga de entorno virtual para el funcionamiento

--- condiciones.py
+++
@@ -1,9 +0,0 @@
-edad = 20
-if edad < 18:
-    print("NO puedes entrar")
-elif edad>= 21:
-    print("puedes entrar y tambien puedes beber")
-else:
-    print("puedes entrar al bar pero no puedes beber")
-
-
