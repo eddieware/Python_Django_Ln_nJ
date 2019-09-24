2ebd441326d5342c40205028a4bb34df80743847
Eduardo <ing.eduardo.mejia.bautista@gmail.com>
2019-09-24 08:02:01 -0500

    METODOS_Y_LISTAS

--- Material_programadores/revomiendo_elementos_de_listas.py
+++ Material_programadores/revomiendo_elementos_de_listas.py
@@ -3,6 +3,12 @@
 # numeros.remove(2)
 #
 # print(numeros)
+#METODO POP!!!!!!!!!!!!!!!!!!!
+#numeros = [1,2,3,4,5]
+# numeros.pop(1)
+#METODO EXTEND PARA AGREGAR ELEMENTSO A LA LISTA
+#numeros = [1,2,3,4,5]
+# numeros_20=[6,7,8,9,11,12,13,14,15,16,17,18,19,20]
+# numeros.extend(numeros_20)
+# print(numeros)
 
-numeros = [1,2,3,4,5]
-numeros.pop(1)
