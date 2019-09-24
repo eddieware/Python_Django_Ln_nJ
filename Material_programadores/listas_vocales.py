9e3ebc84612e7c4ab564e66504f4d6ff01efe64f
Eduardo <ing.eduardo.mejia.bautista@gmail.com>
2019-09-24 07:34:33 -0500

    vocales que no se repiten

--- Material_programadores/listas_vocales.py
+++ Material_programadores/listas_vocales.py
@@ -1,6 +1,9 @@
 vocales=['a','e','i','o','u']
-palabra='aldo'
-
-for i in palabra:
-    if i in vocales:
-        print(i)
+palabra='mama'
+vocales_encontradas=[]
+for ch in palabra:
+    if ch in vocales:
+        if ch not in vocales_encontradas:
+            vocales_encontradas.append(ch)# append es para añadir a la lista las vocales encontradas a la lista
+for vocal in vocales_encontradas:
+    print(vocal)
