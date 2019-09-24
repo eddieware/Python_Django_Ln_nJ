3aeeea445d9e23df087a4b7c0ed3c6964e5ccbdb
Eduardo <ing.eduardo.mejia.bautista@gmail.com>
2019-09-24 08:05:09 -0500

    METODOS_INSERT

--- Material_programadores/revomiendo_elementos_de_listas.py
+++ Material_programadores/revomiendo_elementos_de_listas.py
@@ -10,5 +10,8 @@
 #numeros = [1,2,3,4,5]
 # numeros_20=[6,7,8,9,11,12,13,14,15,16,17,18,19,20]
 # numeros.extend(numeros_20)
-# print(numeros)
+# print
+numeros = [1,2,3,4,5]
+numeros.insert(0,2)# insert es para insertar un caracter en una posicion
+print(numeros)
 
