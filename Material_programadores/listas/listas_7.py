c7c62ba513fd4981f590264f591aecec676994ee
Eduardo Mejia Bautista <ing.eduardo.mejia.bautista@gmail.com>
2019-10-05 13:44:44 -0500

    listas_abc

---
+++ Material_programadores/listas/listas_7.py
@@ -0,0 +1,12 @@
+letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u',
+          'v', 'w', 'x', 'y', 'z']
+Sizeletras = len(letras)
+print(Sizeletras)
+############
+listanumeros = list(enumerate(letras, 1))
+# print(range(,Sizeletras+1))
+print(list(range(1, Sizeletras + 1)))
+# print(letras)
+
+# for n in range (Sizeletras):
+#     print(" el numero asignado a la letra: ",letras[n],"no es divisible entre 3: ",listanumeros[n])
--- Material_programadores/listas/listas_7.pyi
+++
@@ -1,4 +0,0 @@
-letras=["a","b","c","d","e","f","g"]
-
-for letra in letras:
-    print(letra)
